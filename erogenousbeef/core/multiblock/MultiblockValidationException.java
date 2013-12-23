ae91145fedce9f23761cc39d078ac57e8e5ff6b0
erogenousbeef <erogenousbeef@gmail.com>
2013-12-23 01:31:14 +0100

    Fixing compile problems with Forge 916

--- erogenousbeef/core/multiblock/MultiblockValidationException.java
+++ erogenousbeef/core/multiblock/MultiblockValidationException.java
@@ -10,13 +10,4 @@ public class MultiblockValidationException extends Exception {
 	public MultiblockValidationException(String reason) {
 		super(reason);
 	}
-
-	public MultiblockValidationException(String reason, Throwable arg1) {
-		super(reason, arg1);
-	}
-
-	public MultiblockValidationException(String reason, Throwable arg1,
-			boolean arg2, boolean arg3) {
-		super(reason, arg1, arg2, arg3);
-	}
 }
